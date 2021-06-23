6ff7806f2be84d25fca85939a95312d63ac17876
dhrutip <dhruti.pandya@gmail.com>
2021-06-23 16:11:09 -0700

    users can use app in portrait and landscape mode

--- app/src/main/java/com/example/flixter/models/Movie.java
+++ app/src/main/java/com/example/flixter/models/Movie.java
@@ -9,11 +9,13 @@ import java.util.List;
 
 // encapsulate the idea of a movie
 public class Movie {
+    String backdropPath;
     String posterPath;
     String title;
     String overview;
 
     public Movie(JSONObject jsonObject) throws JSONException {
+        backdropPath = jsonObject.getString("backdrop_path");
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
@@ -39,6 +41,10 @@ public class Movie {
 
     public String getOverview() {
         return overview;
+    }
+
+    public String getBackdropPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", backdropPath);
     }
 }
 
